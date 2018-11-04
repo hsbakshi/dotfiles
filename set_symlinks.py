c87be8d68f57ce649a970d0d614c6bd91ed77fee
Hrishi Bakshi <bakshi.hrishikesh@gmail.com>
2018-11-03 23:17:09 -0400

    Upgrade to python 3

--- set_symlinks.py
+++ set_symlinks.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """ 
     This script symlinks default linux settings files 
     to files in this directory.
@@ -28,20 +28,21 @@ def set_symlink(fname):
     home_dir = os.path.expanduser("~")
     default_path = os.path.join(home_dir, "."+fname)
     repo_path = os.path.join(sys.path[0], fname)
-    print 'Setting symlink for', fname, '...',
+    print('Setting symlink for', fname, '...',)
     if os.path.exists(default_path):
         os.remove(default_path)
     try:
         os.symlink(repo_path, default_path)
-        print 'done.'
+        print('done.')
     except Exception as e:
-        print '\nUnable to create symlink for', fname
-        print type(e)
-        print e.args,'\n'
+        print('\nUnable to create symlink for', fname)
+        print(type(e))
+        print(e.args,'\n')
 
 if __name__ == "__main__":
     files = get_file_list()
     for fname in files:
+        fname = fname.decode("utf-8")
         fname = fname.rstrip()
         if fname not in EXCLUDES:
             set_symlink(fname)
